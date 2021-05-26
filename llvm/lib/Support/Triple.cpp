85412df2f548f2f2ce3e9d568d8a9fe21f3aa6f8
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-05-26 14:56:29 -0500

    Typo

--- llvm/lib/Support/Triple.cpp
+++ llvm/lib/Support/Triple.cpp
@@ -192,7 +192,7 @@ StringRef Triple::getOSTypeName(OSType Kind) {
   case AMDHSA: return "amdhsa";
   case AMDPAL: return "amdpal";
   case Ananas: return "ananas";
-  case AstraeaOS return "astraeaos";
+  case AstraeaOS: return "astraeaos";
   case CUDA: return "cuda";
   case CloudABI: return "cloudabi";
   case Contiki: return "contiki";
