ba1b9ec96336ae1545a1375e134e89c443d6a4ce
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-05-26 10:47:46 -0500

    Added some more features, testing it out as I go

--- clang/lib/Driver/ToolChains/AstraeaOS.h
+++ clang/lib/Driver/ToolChains/AstraeaOS.h
@@ -1,4 +1,4 @@
-//===--- AstraeaOS.h - AstraeaOS ToolChain Implementations -------*- C++ -*-===//
+//===--- AstraeaOS.h - AstraeaOS ToolChain Implementations ------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -9,18 +9,66 @@
 #define LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_ASTRAEAOS_H
 
 #include "Gnu.h"
-#include "clang/Driver/Toolchain.h"
+#include "clang/Driver/Tool.h"
+#include "clang/Driver/ToolChain.h"
 
 namespace clang {
-  namespace driver {
-    namespace toolchains {
-      
-class LLVM_LIBRARY_VISIBILITY AstraeaOS : public Generic_ELF {
-  public:
-    AstraeaOS(const Driver &D, const llvm::Triple &Triple,
-              const llvm::opt::ArgList &ARgs);
-  
-  protected:
-    Tool *buildAssembler() const override;
-    Tool *buildLinker() const override;
-};
+namespace driver {
+namespace tools {
+namespace AstraeaOS {
+class LLVM_LIBRARY_VISIBILITY Linker : public Tool {
+public:
+  Linker(const ToolChain &TC) : Tool("astraeaos::Linker", "ld.lld", TC) {}
+
+  bool hasIntegratedCPP() const override;
+  bool isLinkJob() const override;
+  bool isMathErrnoDefault() const override;
+  bool useRelaxRelocations() const override;
+  RuntimeLibType GetDefaultRuntimeLibType() const override {
+    return ToolChain::RLT_CompilerRT;
+  }
+
+  void ConstructJob(Compilation &C, const JobAction &JA,
+                    const InputInfo &Output, const InputInfoList &Inputs,
+                    const llvm::opt::ArgList &TCArgs,
+                    const char *LinkingOutput) const override;
+}
+} // namespace AstraeaOS
+} // namespace tools
+} // namespace driver
+} // namespace clang
+
+namespace toolchains {
+class LLVM_LIBRARY_VISIBILITY AstraeaOS : public ToolChain {
+public:
+  AstraeaOS(const Driver &D, const llvm::Triple &Triple,
+            const llvm::opt::ArgList &Args);
+
+  bool HasNativeLLVMSupport() const override;
+  bool IsIntegratedAssemblerDefault() const override;
+
+  void
+  AddClangSystemIncludeArgs(const llvm::opt::ArgList &DriverArgs,
+                            llvm::opt::ArgStringList &CC1Args) const override;
+
+  void
+  addLibStdCxxIncludePaths(const llvm::opt::ArgList &DriverArgs,
+                           llvm::opt::ArgStringList &CC1Args) const override;
+
+  RuntimeLibType
+  GetRuntimeLibType(const llvm::opt::ArgList &Args) const override;
+  UnwindLibType GetUnwindLibType(const llvm::opt::ArgList &Args) const override;
+
+  void
+  addClangTargetOptions(const llvm::opt::ArgList &DriverArgs,
+                        llvm::opt::ArgStringList &CC1Args,
+                        Action::OffloadKind DeviceOffloadKind) const override;
+
+  const char *getDefaultLinker() const override{return "ld.lld"};
+
+protected:
+  Tool *buildAssembler() const override;
+  Tool *builderLinker() const override;
+}
+} // namespace toolchains
+#endif // LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_ASTRAEAOS_H
